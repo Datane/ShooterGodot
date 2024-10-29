82280cbb4d264c2a955ab2b8f4c9a3f36dfffb0d
alaneslaleche123 <eltimsr.1234@gmail.com>
2024-10-29 23:56:44 +0000

    boton de pausa completo
    arregle el boton de pausa ya esta listo

--- Node.gd
+++ Node.gd
@@ -5,6 +5,7 @@ func delay(seconds):
 	await get_tree().create_timer(seconds).timeout
 # Inicialización del nodo
 func _ready():
+
 	$".".visible = false  # Ocultar el nodo de interfaz al inicio
 	#process_mode = Node.PROCESS_MODE_PAUSABLE  # Permitir pausar el proceso
 # Lógica del juego en cada frame
@@ -12,6 +13,8 @@ func _physics_process(delta):
 	# Si se presiona la tecla "Esc", alternar entre pausar y reanudar
 	if Input.is_action_just_pressed("ui_cancel"):
 		toggle_pause()
+		
+
 # Función para alternar entre pausar y reanudar el juego
 func toggle_pause():
 	get_tree().paused = not get_tree().paused  # Cambiar el estado de pausa
@@ -22,10 +25,13 @@ func toggle_pause():
 		$ColorRect.visible = true  # Mostrar la interfaz de pausa
 		$".".visible = true  # Mostrar otros elementos de la UI (si es necesario)
 	else:
-		# Ocultar el cursor y reanudar el control del juego
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		$ColorRect.visible = false  # Ocultar la interfaz de pausa
-		$".".visible = false  # Ocultar otros elementos de la UI
+		audio_stream_player_2d_2.play()
+		#funcion de delay
+		await delay(0.5)  # Espera 2 segundos
+		$".".visible = false  # Ocultar la UI
+		$ColorRect.visible = false  # Ocultar también el ColorRect
+		get_tree().paused = false  # Reanudar el juego
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)  # Capturar el cursor para volver al juego
 # Lógica cuando se presiona un botón de la UI
 func _on_button_pressed():
 	audio_stream_player_2d_2.play()
